7bc3ca177b2add9df0e3aba76054d12d46dcec2d
Nikita <mrbushik1@gmail.com>
2022-09-03 16:11:41 +0300

    ready pagination module

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -1,31 +1,64 @@
-import React from 'react';
-import _ from 'lodash';
+import React, { useState } from 'react';
+import _, { set } from 'lodash';
 import './pagination.scss';
 import { Arrow } from '../../icons';
 function Pagination({ itemsCount, pageSize, onPageChange, currentPage }) {
+  const [pageSwitch, setPageSwitch] = React.useState(currentPage);
   const pageCount = Math.ceil(itemsCount / pageSize);
   if (pageCount === 1) return null;
   const pages = _.range(1, pageCount + 1);
-  console.log(pages);
+
+  const viewNextPage = () => {
+    if (pageSwitch < 4) {
+      setPageSwitch((pervState) => pervState + 1);
+    }
+  };
+  const viewPerviusPage = () => {
+    if (pageSwitch >= 2) {
+      setPageSwitch((pervState) => pervState - 1);
+    }
+  };
+
+  const cutPagesArray = (numberPage) => {
+    console.log('number' + numberPage);
+    if (pages.length === 3 || pages.length < 3) {
+      return pages;
+    } else {
+      return pages.slice(numberPage - 1, numberPage + 2);
+    }
+  };
+  const targetPages = cutPagesArray(pageSwitch);
+  console.log(targetPages);
   return (
     <nav>
-      <ul>
-        <li>
+      <ul className="pagination-list">
+        <li onClick={viewPerviusPage}>
           {' '}
           <Arrow />
         </li>
-        {pages.map((page) => (
-          <li className={page === currentPage ? 'pagination-active' : ''} key={'page ' + page}>
-            <a onClick={() => onPageChange(page)}>{page}</a>
+        {targetPages.map((page) => (
+          <li
+            onClick={() => onPageChange(page)}
+            className={page === currentPage ? 'pagination-active' : ''}
+            key={'page ' + page}>
+            <a>{page}</a>
           </li>
         ))}
-        {pages.length > 3 ? <li>{pages[pages.length - 1]}</li> : ''}
+        <li className="pagination-plug">...</li>
+        {pages.length > 3 ? (
+          <li onClick={() => onPageChange(pages[pages.length - 1])}>{pages[pages.length - 1]}</li>
+        ) : (
+          ''
+        )}
 
-        <li>
+        <li onClick={viewNextPage}>
           {' '}
           <Arrow />
         </li>
       </ul>
+      <div className="">
+        {currentPage * 4 - 4}-{currentPage * 4} of {itemsCount}
+      </div>
     </nav>
   );
 }
